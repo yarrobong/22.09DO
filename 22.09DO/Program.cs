c5f7a244e83a0242e984e89f0957329043948134
yarrobong <oneboardsholi@gmail.com>
2023-09-24 12:54:20 +0500

    Добавьте файлы проекта.

---
+++ 22.09DO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите координаты точки (x; y):");
+        Console.Write("x = ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.Write("y = ");
+        double y = double.Parse(Console.ReadLine());
+
+       var aaa = y <= Math.Exp(x) && x * x + Math.Pow(y - 3, 2) <= 4;
+       var bbb = y <= Math.Exp(x) && (x - 1) * (x - 1) + y * y <= 4;
+        if (aaa && bbb)
+            Console.WriteLine("YES");
+        else
+            Console.WriteLine("NO");
+    }
+}
